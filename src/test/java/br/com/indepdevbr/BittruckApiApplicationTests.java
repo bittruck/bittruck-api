a141e7097c3869a0e466a5fc73c2449bdd625879
Batman <work.leao@gmail.com>
2019-10-13 19:20:20 -0300

    Configuração completa para uso de JWT, H2DB em memoria

---
+++ src/test/java/br/com/indepdevbr/BittruckApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.indepdevbr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BittruckApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
