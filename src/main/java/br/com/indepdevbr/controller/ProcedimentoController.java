5cd36d0326ab601097bc091ce35e4add12136ac1
Batman <work.leao@gmail.com>
2019-10-16 22:25:54 -0300

    Adicionando procedimento no controller

--- src/main/java/br/com/indepdevbr/controller/ProcedimentoController.java
+++ src/main/java/br/com/indepdevbr/controller/ProcedimentoController.java
@@ -1,5 +1,29 @@
 package br.com.indepdevbr.controller;
 
-public class ProcedimentoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.indepdevbr.models.Procedimento;
+import br.com.indepdevbr.services.imp.ProcedimentoServiceImp;
+
+@RestController
+@RequestMapping("/procedimento")
+public class ProcedimentoController {
+	
+	@Autowired
+	private ProcedimentoServiceImp procedimentoServiceImp;
+	
+	public Procedimento buscarPorId(@PathVariable("idProcedimento") Long idProcedimento) {
+		return procedimentoServiceImp.buscarPorId(idProcedimento);
+	}
+	
+	
+	public List<Procedimento> listaTodos() {
+		return procedimentoServiceImp.listarTodos();
+	}
+	
 }
