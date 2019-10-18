b05d7b2442a81bcf477a84a2a069bc62fb8d96f9
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2019-10-18 16:47:35 -0300

    Adicionando autenticação para usar swagger

--- src/main/java/br/com/indepdevbr/sec/SwaggerConfig.java
+++ src/main/java/br/com/indepdevbr/sec/SwaggerConfig.java
@@ -1,17 +1,31 @@
 package br.com.indepdevbr.sec;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
+import io.swagger.models.auth.In;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	/*
+	 * private ApiKey apiKey() { return new ApiKey("apikey", "Authorization",
+	 * "header"); }
+	 */
 
 	@Bean
 	public Docket api() {
@@ -20,7 +34,25 @@ public class SwaggerConfig {
 					.apis(RequestHandlerSelectors.basePackage("br.com.indepdevbr.controller"))
 					.paths(PathSelectors.any())					
 					.build()
-					.useDefaultResponseMessages(false);					
+					.useDefaultResponseMessages(false)
+					 .securitySchemes(Arrays.asList(new ApiKey("Token Access", HttpHeaders.AUTHORIZATION, In.HEADER.name())))
+				        .securityContexts(Arrays.asList(securityContext()));
 	}
+	
+	private SecurityContext securityContext() {
+	    return SecurityContext.builder()
+	        .securityReferences(defaultAuth())
+	        .forPaths(PathSelectors.any())
+	        .build();
+	}
+	
+	List<SecurityReference> defaultAuth() {
+	    AuthorizationScope authorizationScope
+	        = new AuthorizationScope("ADMIN", "accessEverything");
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	    authorizationScopes[0] = authorizationScope;
+	    return Arrays.asList(
+	        new SecurityReference("Token Access", authorizationScopes));
+	}	
 
 }
