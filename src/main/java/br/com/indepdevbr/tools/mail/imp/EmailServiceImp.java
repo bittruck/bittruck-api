b06efcd9cd5ccf015b72058c2cc81655e4ffcc9d
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2019-10-25 13:16:51 -0300

    Classe de envio de e-mail

--- src/main/java/br/com/indepdevbr/tools/mail/imp/EmailServiceImp.java
+++ src/main/java/br/com/indepdevbr/tools/mail/imp/EmailServiceImp.java
@@ -1,5 +1,7 @@
 package br.com.indepdevbr.tools.mail.imp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,13 +15,20 @@ public class EmailServiceImp implements IEmailService {
 	@Autowired
 	private JavaMailSender emailSender;
 	
+	private final Logger logger = LogManager.getLogger(EmailServiceImp.class);
+	
 	@Override
 	public void enviarEmail(String assunto, String corpo, String... para) {
-		SimpleMailMessage mailMessage = new SimpleMailMessage();
-		mailMessage.setTo(para);
-		mailMessage.setSubject(assunto);
-		mailMessage.setText(corpo);
-		emailSender.send(mailMessage);
+		try {
+			logger.info("metodo=enviarEmail, para=[" + para + "], assunto=" + assunto + ", corpo=" + corpo);
+			SimpleMailMessage mailMessage = new SimpleMailMessage();
+			mailMessage.setTo(para);
+			mailMessage.setSubject(assunto);
+			mailMessage.setText(corpo);
+			emailSender.send(mailMessage);
+		} catch (Exception e) {
+			logger.error("metodo=enviarEmail, throw=" + e.getLocalizedMessage(), e);
+		}
 	}
 
 }
